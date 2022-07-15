179309044a33f8cd9763bc61238da96305eed175
Gautam <46226462+Gautamprasadsah@users.noreply.github.com>
2022-07-15 07:35:49 +0545

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,24 +1,37 @@
-<?php include "../inc/dbinfo.inc"; ?>
 <?php
-$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
-if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
-
+//conect to database
+$servername = "database-1.ccqtyec2mqcu.ap-south-1.rds.amazonaws.com";
+$username = "admin";
+$password = "admin123";
+$db = "test2";
+$conn = mysqli_connect($servername, $username, $password,$db);
+ 
+//checking connection  
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+else{
+echo "connected";
+}
 //insert
-$name = $_POST['name'];
+$name = $_POST['name']; 
 $email = $_POST['email'];
-$phonenumber = $_POST['phonenumber'];
-$message = $_POST['message'];
-$run = "INSERT INTO `contact`(name, email, phonenumber, message) VALUES ( '$name' , '$email' , '$phonenumber' , '$message')";
+$phonenumber = $_POST['phonenumber']; 
+$message = $_POST['message'];   
+
+$run = "INSERT INTO `test2`(name, email, phonenumber, message) VALUES ( '$name' , '$email' , '$phonenumber' , '$message' )";
 $result = mysqli_query( $conn , $run) ;
 if($result)
 {
-echo "<br><br><br><br><br><br><br><br><h1 align='center'>Your Details are Submitted...!!</h1>";
-echo "<br><br><br><br><br><br><br><br><h1 align='center'>Thank You...!!</h1>";
-header( "refresh:3;url=index.html" );
+	echo "<br><br><br><br><br><br><br><br><h1 align='center'>Yours Details are Submitted...!!</h1>";
+        echo "<br><br><br><br><br><br><br><br><h1 align='center'>Thank You...!!</h1>";
+        header( "refresh:3;url=contact.html" );
+    
 }
 else
 {
-echo "not registered";
-header( "refresh:3;url=index.html" );
-}
+	echo "not registered";
+        header( "refresh:3;url=contact.html" );
+} 
+
 ?>
